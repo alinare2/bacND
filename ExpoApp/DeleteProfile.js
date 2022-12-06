b6273672abb2ec6123f8c2a677ce76e57e1d2b4a
ppenderg <ppenderg@nd.edu>
2022-12-05 21:31:43 -0500

    Added delete thing

--- ExpoApp/DeleteProfile.js
+++ ExpoApp/DeleteProfile.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, MaskedViewBase} from 'react-native';
 import Axios from 'axios';
 import {useNavigate} from 'react-router-native';
 
+import { useAuth } from './AuthContext';
 
 export default function DeleteProfile(){
 
+    const {currentUser, setCurrentUser, setIsLoggedIn} = useAuth();
+
+    const nav = useNavigate();
+
     function handleDelete(e){
-        console.log("AHHHHHHHH");
+    
+        let payload = {'user_id' : currentUser}
+        Axios.post(`http://db8.cse.nd.edu/cse30246/server/bacND/postDeleteAccount.php`, payload)
+        .then((response)=>{
+            setCurrentUser(-1);
+            setIsLoggedIn(false); 
+            nav("/");
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    
     }
 
     return (
         <View>
             <Text> Are you sure you want to delete your account?</Text>
-            <Button title="yes" onPress={handleDelete}/>
+            <Button title="Yes" onPress={handleDelete}/>
+            <Button title="Oh no. No, no, no, no, no, no, no! Wait, wait, wait, wait, hey. Wait, wait, wait, wait!" onPress={nav("/")}/>
         </View>
     );
 }
